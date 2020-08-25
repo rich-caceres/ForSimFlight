feca0be275e8f82e1a35aa1ba5f5bacb70f6dd8a
rich-caceres <richard.caceres22590@outlook.com>
2020-08-25 13:44:32 -0400

    Using string and append for user name

--- HelloWorldFirstApp/HelloWorldFirstApp/HelloWorldFirstApp.cpp
+++ HelloWorldFirstApp/HelloWorldFirstApp/HelloWorldFirstApp.cpp
@@ -2,12 +2,16 @@
 //
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
     int num = 5;
     int userNum = 0;
+    string firstName = "";
+    string lastName = "";
+
 
     cout << "Hello World!\n";
     cout << "Working with C++ is fun\nmy number is equal to " << num << "\nnum can also be printed with another statement like so: \n";
@@ -15,11 +19,16 @@ int main()
     cout << "\nLets get user input, please enter your age:\n";
     cin >> num;
     cout << "Your age is " << num << " thats pretty awesome. Thanks for telling me!";
-    cout << "now enter a number so that C++ can do math";  
+    cout << "now enter a number so that C++ can do math\n";  
     cin >> userNum;
     cout << userNum << " + " << num << " = " << num + userNum;
     
-    
+    cout << "\nOk, let's have C++ get your name. Please enter your first name:\n ";
+    cin >> firstName;
+    cout << "enter your last name:\n ";
+    cin >> lastName;
+    cout << "Hello " << firstName.append(" ").append(lastName) << ", it's very nice to meet you!";
+
     return 0;
 }
 
