3e8f588e3257934f233271596be8a58eb50740a2
rich-caceres <richard.caceres22590@outlook.com>
2021-08-08 13:50:18 -0400

    removing includes thata are not being used in header file

--- FavoriteNum/FavoriteNum/Header.h
+++ FavoriteNum/FavoriteNum/Header.h
@@ -1,11 +1,7 @@
 #pragma once
-#include "iostream"//needed for input output
-#include "string"//using strings
-#include "vector"//using vectors
-#include "cmath"//math library
-#include "cstdlib"//C standard library
-#include "iomanip"//setting Precision
-#include "array"//using array library
+#include <iostream>//needed for input output
+#include <vector>//using vectors
+#include <array>//using array library
 
 	class Header 
 	{
--- FavoriteNum/FavoriteNum/Source.cpp
+++ FavoriteNum/FavoriteNum/Source.cpp
@@ -9,13 +9,13 @@
 */
 
 
-#include "iostream"//needed for input output
-#include "string"//using strings
-#include "vector"//using vectors
-#include "cmath"//math library
-#include "cstdlib"//C standard library
-#include "iomanip"//setting Precision
-#include "array"//using array library
+#include <iostream>//needed for input output
+#include <string>//using strings
+#include <vector>//using vectors
+#include <cmath>//math library
+#include <cstdlib>//C standard library
+#include <iomanip>//setting Precision
+#include <array>//using array library
 #include "Header.h"
 
 
