8bbbd05fac7cfe4c1837d99b50cc2becf6731aef
Richard Caceres <richard.caceres22590@outlook.com>
2021-08-19 07:59:42 -0400

    included iostream

--- MovieCollection/MovieCollection/Movie.cpp
+++ MovieCollection/MovieCollection/Movie.cpp
@@ -1,7 +1,9 @@
-#include "Movie.h"
+#include "Movie.h"\
+#include <iostream>
 
 Movie::Movie() 
 	:movie_name{ "No name added" }, movie_rating{ '\0' }, num_watched{0} {
+	
 	movie_name = "";
 	movie_rating= '\0';
 	num_watched = 0;
