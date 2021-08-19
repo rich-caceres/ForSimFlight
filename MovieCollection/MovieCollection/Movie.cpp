e330502d343dacc185b3b2176fa71d43e32a9ba4
Richard Caceres <richard.caceres22590@outlook.com>
2021-08-19 07:59:08 -0400

    added list initialization to default constructor

--- MovieCollection/MovieCollection/Movie.cpp
+++ MovieCollection/MovieCollection/Movie.cpp
@@ -1,6 +1,7 @@
 #include "Movie.h"
 
-Movie::Movie() {
+Movie::Movie() 
+	:movie_name{ "No name added" }, movie_rating{ '\0' }, num_watched{0} {
 	movie_name = "";
 	movie_rating= '\0';
 	num_watched = 0;
