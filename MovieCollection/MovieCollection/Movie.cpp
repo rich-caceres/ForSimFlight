017824ee076a3d8534db0d423a5976c6c52dbfc8
Richard Caceres <richard.caceres22590@outlook.com>
2021-08-19 08:04:56 -0400

    added cin for movie rating

--- MovieCollection/MovieCollection/Movie.cpp
+++ MovieCollection/MovieCollection/Movie.cpp
@@ -1,11 +1,13 @@
-#include "Movie.h"\
+#include "Movie.h"
 #include <iostream>
 
 Movie::Movie() 
 	:movie_name{ "No name added" }, movie_rating{ '\0' }, num_watched{0} {
+	std::cout << "Please add the name of the movie:\n";
+	std::cin >> movie_name;
+	std::cout << "Please add the movie rating: \n";
+	std::cin >> movie_rating;
 	
-	movie_name = "";
-	movie_rating= '\0';
 	num_watched = 0;
 }
 
