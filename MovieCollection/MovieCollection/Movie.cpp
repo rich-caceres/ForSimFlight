9e0fc1bf8eece72b5b1b0830b454c47e4d3f4840
Richard Caceres <richard.caceres22590@outlook.com>
2021-08-19 12:15:30 -0400

    added list initialization to constructor

--- MovieCollection/MovieCollection/Movie.cpp
+++ MovieCollection/MovieCollection/Movie.cpp
@@ -1,7 +1,8 @@
 #include "Movie.h"
 #include <iostream>
 
-Movie::Movie() {
+Movie::Movie() 
+	: movie_name{ "No name added" }, movie_rating{ '\0' }, num_watched{0} {
 	std::cout << "Please add the name of the movie:\n";
 	std::cin >> movie_name;
 	std::cout << "Please add the movie rating: \n";
