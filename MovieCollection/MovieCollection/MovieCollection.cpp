0533c9f4c0c1dd83fd6bc46b759eb559161d056d
rich-caceres <richard.caceres22590@outlook.com>
2021-08-20 10:20:04 -0400

    implemented the movie method to main

--- MovieCollection/MovieCollection/MovieCollection.cpp
+++ MovieCollection/MovieCollection/MovieCollection.cpp
@@ -10,17 +10,9 @@ Movie createMovie();
 
 int main()
 {
-	std::string movie_name{""};
-	std::string movie_rating{""};
-	int num_watched{0};
 
-	std::cout << "Please add the name of the movie:\n";
-	std::getline(std::cin, movie_name);
-	std::cout << "Please add the movie rating: \n";
-	std::getline(std::cin, movie_rating);
-	std::cout << "Please add the number of times watched: \n";
-	std::cin >> num_watched;
-	Movie movie(movie_name, movie_rating, num_watched);
+	Movie movie = createMovie();
+
 	movie.displayMovie();
 }
 
