284b88fdc3bcf68827688f51249e505216f19fd2
rich-caceres <richard.caceres22590@outlook.com>
2021-08-20 10:25:28 -0400

    added variables that will be passed to setter methods in prototypes

--- MovieCollection/MovieCollection/Movie.h
+++ MovieCollection/MovieCollection/Movie.h
@@ -12,8 +12,8 @@ public:
 	std::string getMovieRating() const;
 	int getNumWatched() const;
 	void setMovieName(std::string movie_name);
-	void setMovieRating();
-	void setNumWatched();
+	void setMovieRating(std::string movie_rating);
+	void setNumWatched(int num_watched);
 	void displayMovie() const;
 	//constructors
 	Movie(std::string movie_name = "No name given", std::string movie_rating = "no rating/unknown", int num_watched = 0);
