824e6547ab48b965ea9f57bc8683b55398cef52b
rich-caceres <richard.caceres22590@outlook.com>
2021-08-24 11:43:08 -0400

    starting code for op overload

---
+++ OpOverload/OpOverload/MyString.h
@@ -0,0 +1,15 @@
+#pragma once
+class MyString
+{
+	private:
+		char* str;
+	public:
+		MyString();
+		MyString(const char* s);
+		MyString(const MyString& s);
+		~MyString();
+		void display() const;
+		int get_length() const;
+		const char* get_str() const;
+};
+
---
+++ OpOverload/OpOverload/OpOverload.cpp
@@ -0,0 +1,9 @@
+
+
+#include <iostream>
+#include "MyString.h"
+
+int main()
+{
+}
+
