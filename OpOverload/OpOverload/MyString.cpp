cd6d0a0fc5d9ee8603c4730b2aa453dea331f190
rich-caceres <richard.caceres22590@outlook.com>
2021-08-24 12:13:44 -0400

    implementing the constructors and destructors

--- OpOverload/OpOverload/MyString.cpp
+++ OpOverload/OpOverload/MyString.cpp
@@ -1,1 +1,32 @@
+#include <cstring>
+#include <iostream>
 #include "MyString.h"
+
+MyString::MyString()
+	:str(nullptr) {
+	str = new char[1];
+	* str = '\0';
+}
+
+MyString::MyString(const char* s)
+	: str{ nullptr } {
+	if (s == nullptr) {
+		str = new char[1];
+		*str = '\0';
+	}
+	else {
+		str = new char[std::strlen(s) + 1];
+		std::strcpy(str, s);
+	}
+}
+MyString::MyString(const MyString &source) 
+	:str{nullptr} {
+
+	str = new char[std::strlen(source.str) + 1];
+	std::strcpy(str, source.str);
+
+}
+MyString::~MyString() {
+	delete str;
+}
+
