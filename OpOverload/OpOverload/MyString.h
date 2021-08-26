b1db2ca7b3fe0267e2ed9e81e0eab6ba81946306
rich-caceres <richard.caceres22590@outlook.com>
2021-08-26 12:24:22 -0400

    added comments and code for member functions

--- OpOverload/OpOverload/MyString.h
+++ OpOverload/OpOverload/MyString.h
@@ -1,6 +1,7 @@
 #pragma once
 class MyString
 {
+	//friend overloaded operators as global methods
 	friend bool operator==(const MyString& lhs, const MyString& rhs);
 	friend MyString operator-(const MyString& obj);
 	friend MyString operator+(const MyString& lhs, const MyString& rhs);
@@ -20,5 +21,14 @@ class MyString
 		const char* get_str() const;
 		MyString& operator=(const MyString& rhs);//copy assignment
 		MyString& operator=(MyString&& rhs);//move assignment
+		
+
+		//overloaded functions that are member functions
+		// =============================================
+		//bool operator==(const MyString& lhs, const MyString& rhs);
+		//MyString operator-(const MyString& obj);
+		//MyString operator+(const MyString& lhs, const MyString& rhs);
+		//std::ostream& operator<<(std::ostream& os, const MyString& rhs);
+		//std::istream& operator>>(std::istream& in, MyString& rhs);
 };
 
