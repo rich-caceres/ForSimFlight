61d787176141df1d922cab9250c09746973f475d
rich-caceres <richard.caceres22590@outlook.com>
2021-08-26 12:04:19 -0400

    added overload function for insertion and extraction operators

--- OpOverload/OpOverload/MyString.cpp
+++ OpOverload/OpOverload/MyString.cpp
@@ -108,3 +108,17 @@ MyString operator+(const MyString& lhs, const MyString& rhs) {
 	delete[] buff;
 	return temp;
 }
+
+//overloaded insertion operator
+std::ostream& operator<<(std::ostream& os, const MyString& rhs) {
+	os << rhs.str;
+	return os;
+}
+
+std::istream& operator>>(std::istream& in, MyString& rhs) {
+	char* buff = new char[1000];
+	in >> buff;
+	rhs = MyString{ buff };
+	delete[] buff;
+	return in;
+}
--- OpOverload/OpOverload/MyString.h
+++ OpOverload/OpOverload/MyString.h
@@ -4,6 +4,9 @@ class MyString
 	friend bool operator==(const MyString& lhs, const MyString& rhs);
 	friend MyString operator-(const MyString& obj);
 	friend MyString operator+(const MyString& lhs, const MyString& rhs);
+	friend std::ostream& operator<<(std::ostream& os, const MyString& rhs);
+	friend std::istream& operator>>(std::istream& in, MyString& rhs);
+
 	private:
 		char* str;
 	public:
