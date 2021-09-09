e4becade9bec3919da6db3ec734e188d8f1a828d
rich-caceres <richard.caceres22590@outlook.com>
2021-09-08 21:15:57 -0400

    withdraw function has been created

--- BankAccountProj/BankAccountProj/Account.cpp
+++ BankAccountProj/BankAccountProj/Account.cpp
@@ -1,2 +1,35 @@
+#include <iostream>
+#include <string>
 #include "Account.h"
+
+Account::Account()
+	:balance{ 0.0 }, amount{0.0} {
+}
+
+Account::~Account() {
+
+}
+
+void Account::withdraw(double amount) {
+	std::cout << "Withdrawing " << amount << " from account." << std::endl;
+	if (amount <= balance) {
+		balance = balance - amount;
+		std::cout << "Withdrawing " << amount << " from account." << std::endl;
+	}
+	else {
+		std::cout << "You do not have the required funds and will overdraft your account, do you wish to proceed?"<<std::endl;
+		char answer{ '\0' };
+		std::cin >> answer;
+		if (tolower(answer) == 'y') {
+			std::cout << "Your account will be overdrafted!" << std::endl;
+		}
+		else {
+			std::cout << "Withdrawal has been canceled" << std::endl;
+		}
+	}
+}
+
+void Account::deposit(double amount) {
+	std::cout << "depositing " << amount << " to account." << std::endl;
+}
 
