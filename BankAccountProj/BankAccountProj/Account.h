1b1c04c01068d2fe10380cb42379da11cfb415ce
rich-caceres <richard.caceres22590@outlook.com>
2021-09-08 20:56:44 -0400

    added initial code, changed gitignore

---
+++ BankAccountProj/BankAccountProj/Account.h
@@ -0,0 +1,10 @@
+#pragma once
+class Account
+{
+public:
+	double balance;
+	double amount;
+	void deposit(double amount);
+	void withdraw(double amount);
+};
+
---
+++ BankAccountProj/BankAccountProj/SavingsAccount.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Account.h"
+class SavingsAccount : public Account
+{
+public:
+	double interest_rate;
+};
+
