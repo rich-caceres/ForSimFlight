c0e8d1ff30f162027e3300628c63bde5b41f2517
rich-caceres <richard.caceres22590@outlook.com>
2021-09-20 20:33:42 -0400

    adding new variables to account

--- BankAccountProj/BankAccountProj/Account.h
+++ BankAccountProj/BankAccountProj/Account.h
@@ -7,9 +7,9 @@ class Account
 public:
 	double balance;
 	std::string name;
-
+	time_t date_created;
 	Account();
-	Account(std::string name, double balance);
+	Account(std::string name, double balance, time_t date_created);
 	~Account();
 	void deposit(double amount);
 	void withdraw(double amount);
