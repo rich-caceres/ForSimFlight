ea71e8eabbbe6ca3a2ac6e2f0b5f9454dfa47b0f
rich-caceres <richard.caceres22590@outlook.com>
2021-09-24 10:46:03 -0400

    fixing savings account apply interest function

--- BankAccountProj/BankAccountProj/SavingsAccount.cpp
+++ BankAccountProj/BankAccountProj/SavingsAccount.cpp
@@ -12,8 +12,9 @@ SavingsAccount::~SavingsAccount(){
 }
 
 void SavingsAccount::applyInterestRate() {
-
-	this->balance = this->balance + (this->balance * interest_rate) ;
-	std::cout << "Balance after applied interest rate is " << balance << std::endl;
-
+	double tempBalance = getBalance();
+	tempBalance = tempBalance + (tempBalance * interest_rate);
+	setBalance(tempBalance);
+	std::cout << "Balance after applied interest rate is " << getBalance() << std::endl;
+	
 }
