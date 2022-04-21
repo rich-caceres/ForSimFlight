c3cfa9996641fe74f098c00399dc133126a49214
rich-caceres <richard.caceres22590@outlook.com>
2022-04-20 20:32:12 -0400

    comments

--- BankAccountProj/BankAccountProj/SavingsAccount.cpp
+++ BankAccountProj/BankAccountProj/SavingsAccount.cpp
@@ -2,15 +2,18 @@
 #include <ctime>
 #include "SavingsAccount.h"
 
+//constructor based off of inheritance, with a default interest rate
 SavingsAccount::SavingsAccount()
 	:interest_rate{ .06 } {
 	std::cout << "Interest rate has been set too " << interest_rate << std::endl;
 }
 
+//destructor
 SavingsAccount::~SavingsAccount(){
-	std::cout << "Destructor for Savings account called"<< std::endl;
+	//std::cout << "Destructor for Savings account called"<< std::endl;
 }
 
+//setting interest rate and getting inerest rate
 void SavingsAccount::setInterestRate(double interest_rate) {
 	this->interest_rate = interest_rate;
 }
@@ -19,6 +22,7 @@ double SavingsAccount::getInterestRate() {
 	return interest_rate;
 }
 
+//applying the interest, will need to expand to include the time
 void SavingsAccount::applyInterestRate() {
 	double *tempBalance = new double;
 	*tempBalance = getBalance();
